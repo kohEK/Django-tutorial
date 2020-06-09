dd9fc3cabeff431c8b16414e156faf77c7d6b964
KohEK <gogoek90@gmail.com>
2020-06-09 13:14:37 +0900

    feat:add  pagenation,filter

--- 0609tutorial/tutorial0609/board/urls.py
+++ 0609tutorial/tutorial0609/board/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from board import views
 
 urlpatterns = [
     path('board/', views.BoardList.as_view()),
     path('board/<int:pk>/', views.BoardDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
--- 0609tutorial/tutorial0609/board/views.py
+++ 0609tutorial/tutorial0609/board/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from board.serializers import BoardSerializer
-# Create your views here.
-from rest_framework import generics
+from rest_framework import generics, mixins, filters
 
 from board.models import Board
 
 
-class BoardList(generics.ListCreateAPIView):
+class BoardList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'code',]
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class BoardDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Board.objects.all()
-    serializer_class = BoardSerializer
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
